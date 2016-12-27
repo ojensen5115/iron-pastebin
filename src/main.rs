db3bf00b3d28909274f25939fb14a2b30bf10809
Oliver Jensen <ojensen5115@gmail.com>
2016-12-27 13:46:00 +0100

    add DELETE functionality

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 /*
 
 TODO:
-- Add a web form to the index where users can manually input new pastes. Accept the form at POST /. Use format and/or rank to specify which of the two POST / routes should be called.
-- Support deletion of pastes by adding a new DELETE /<id> route. Use PasteID to validate <id>.
+- Add a web form to the index where users can manually input new pastes. Accept the form at POST /.
 - Limit the upload to a maximum size. If the upload exceeds that size, return a 206 partial status code. Otherwise, return a 201 created status code.
-- Require that the key is present and matches when doing deletion. Use one of Rocket’s core traits to do the key validation.
 - Add a PUT /<id> route that allows a user with the key for <id> to replace the existing paste, if any.
 - Add a new route, GET /<id>/<lang> that syntax highlights the paste with ID <id> for language <lang>. If <lang> is not a known language, do no highlighting. Possibly validate <lang> with FromParam.
 - Use the testing module to write unit tests for your pastebin.
-- Dispatch a thread before launching Rocket in main that periodically cleans up idling old pastes in upload/.
+- Dispatch a thread before launching Iron in main that periodically cleans up idling old pastes in upload/.
 
 DONE:
+- Ensure generated PasteID is unique.
 - Set the Content-Type of the return value in upload and retrieve to text/plain.
-- Return a unique “key” after each upload
+- Support deletion of pastes by adding a new DELETE /<id> route.
+- Require that the key is present and matches when doing deletion.
 
 */
 
@@ -23,6 +23,7 @@ extern crate bodyparser;
 extern crate rand;
 use rand::Rng;
 
+use std::fs;
 use std::fs::File;
 use std::path::Path;
 use std::io::Write;
@@ -37,6 +38,7 @@ fn main() {
     let mut router = Router::new();
     router.get("/", usage, "index");
     router.get("/:paste_id", retrieve, "retrieve");
+    router.delete("/:paste_id", delete, "delete");
     router.post("/", submit, "submit");
 
     println!("http://localhost:3000/");
@@ -52,11 +54,17 @@ fn usage(_: &mut Request) -> IronResult<Response> {
           accepts raw data in the body of the request and responds with a URL of
           a page containing the body's content:
 
-          echo \"hello world\" | curl --data-binary @- http://localhost:3000
+          eg: echo \"hello world\" | curl --data-binary @- http://localhost:3000
 
       GET /<id>
 
-          retrieves the content for the paste with id `<id>`")))
+          retrieves the content for the paste with id `<id>`
+
+      DELETE /<id>
+
+          deletes the paste with id `<id>`.
+
+          eg: curl -X DELETE http://localhost:3000/fZWK3")))
 }
 
 const BASE62: &'static [u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -89,6 +97,17 @@ fn retrieve(req: &mut Request) -> IronResult<Response> {
     let mut buffer = String::new();
     itry!(f.read_to_string(&mut buffer));
     Ok(Response::with((status::Ok, buffer)))
+}
+
+fn delete(req: &mut Request) -> IronResult<Response> {
+    let ref id = req.extensions.get::<Router>()
+           .unwrap().find("paste_id").unwrap_or("/");
+    let path = format!("uploads/{id}", id = id);
+    if !Path::new(&path).exists() {
+        return Ok(Response::with((status::Ok, format!("Paste {} does not exist.\n", id))));
+    }
+    itry!(fs::remove_file(path));
+    Ok(Response::with((status::Ok, format!("Paste {} deleted.\n", id))))
 }
 
 fn generate_id(size: usize) -> String {
